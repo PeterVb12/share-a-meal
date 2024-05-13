eb39255ba558124c6e96515925c4a81e07552821
Peter <pmh.vanberkum@student.avans.nl>
2024-05-13 17:10:46 +0200

    kleine DAO update

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,20 @@
 const database = require('../dao/inmem-db')
+const logger = require('../util/logger')
+
+const db = require('../dao/mysql-db')
 
 const userService = {
     create: (user, callback) => {
+        logger.info('create user', user)
         database.add(user, (err, data) => {
             if (err) {
+                logger.info(
+                    'error creating user: ',
+                    err.message || 'unknown error'
+                )
                 callback(err, null)
             } else {
+                logger.trace(`User created with id ${data.id}.`)
                 callback(null, {
                     message: `User created with id ${data.id}.`,
                     data: data
@@ -14,90 +23,79 @@ const userService = {
         })
     },
 
-    updateUser: (userId, user, callback) => {
-        database.updateUser(userId, user, (err, data) => {
-            if (err) {
-                callback(err, null)
-            } else {
-                callback(null, {
-                    message: `User updated with id ${data.id}.`,
-                    data: data
-                })
-            }
-        })
-    },
-
-    
-    getById: (userId, callback) => {
-        database.getById(userId, (err, data) => {
-            if (err) {
-                callback(err, null)
-            } else {
-                callback(null, {
-                    message: `User updated with id ${data.id}.`,
-                    data: data
-                })
-            }
-        })
-    },
-
-    delete: (userId, callback) => {
-        database.delete(userId, (err, data) => {
-            if (err) {
-                callback(err, null)
-            } else {
-                callback(null, {
-                    message: `User updated with id ${data.id}.`,
-                    data: data
-                })
-            }
-        })
-    },
-
-
     getAll: (callback) => {
-        database.getAll((err, data) => {
+        logger.info('getAll')
+
+        // Deprecated: de 'oude' manier van werken, met de inmemory database
+        // database.getAll((err, data) => {
+        //     if (err) {
+        //         callback(err, null)
+        //     } else {
+        //         callback(null, {
+        //             message: `Found ${data.length} users.`,
+        //             data: data
+        //         })
+        //     }
+        // })
+
+        // Nieuwe manier van werken: met de MySQL database
+        db.getConnection(function (err, connection) {
             if (err) {
+                logger.error(err)
                 callback(err, null)
-            } else {
-                console.log(data)
-                callback(null, {
-                    message: `Found ${data.length} users.`,
-                    data: data
-                })
+                return
             }
+
+            connection.query(
+                'SELECT id, firstName, lastName FROM `user`',
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (error) {
+                        logger.error(error)
+                        callback(error, null)
+                    } else {
+                        logger.debug(results)
+                        callback(null, {
+                            message: `Found ${results.length} users.`,
+                            data: results
+                        })
+                    }
+                }
+            )
         })
     },
 
-    getAllActive: (callback) => {
-        database.getAllActive((err, data) => {
-            if (err) {
-                callback(err, null)
-            } else {
-                console.log(data)
-                callback(null, {
-                    message: `Found ${data.length} users.`,
-                    data: data
-                })
-            }
-        })
-    },
+    getProfile: (userId, callback) => {
+        logger.info('getProfile userId:', userId)
 
-    getAllInactive: (callback) => {
-        database.getAllInactive((err, data) => {
+        db.getConnection(function (err, connection) {
             if (err) {
+                logger.error(err)
                 callback(err, null)
-            } else {
-                console.log(data)
-                callback(null, {
-                    message: `Found ${data.length} users.`,
-                    data: data
-                })
+                return
             }
-        })
-    },
 
-    
+            connection.query(
+                'SELECT id, firstName, lastName FROM `user` WHERE id = ?',
+                [userId],
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (error) {
+                        logger.error(error)
+                        callback(error, null)
+                    } else {
+                        logger.debug(results)
+                        callback(null, {
+                            message: `Found ${results.length} user.`,
+                            data: results
+                        })
+                    }
+                }
+            )
+        })
+    }
 }
 
 module.exports = userService
